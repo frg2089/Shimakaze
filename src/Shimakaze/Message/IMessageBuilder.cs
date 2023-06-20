1d50910e77428f4a77294e12a96d21d189db297e
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-20 13:46:25 +0800

    :fire: 暂时去掉发送图片的功能
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze/Message/IMessageBuilder.cs
+++ src/Shimakaze/Message/IMessageBuilder.cs
@@ -7,8 +7,6 @@ public interface IMessageBuilder
     IMessageBuilder Add(IMessageBlock span);
     IMessageBuilder Text(string message);
     IMessageBuilder At(string id);
-    IMessageBuilder Image(byte[] data);
-    IMessageBuilder Image(string filePath) => Image(File.ReadAllBytes(filePath));
     IMessageBuilder Face(int faceId);
     IMessageBuilder Clear();
     IMessage Build();
